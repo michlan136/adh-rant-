44906b4ab4a2b8cd96fe78e2c746227fd876e6cc
michlan136 <soulaimanisalma@gmail.com>
2026-05-21 20:55:41 +0100

    Update API destination to use dynamic URL
    Make API destination dynamic based on environment variable.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,10 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
+    // On récupère l'adresse de ton backend sur Render. 
+    // S'il n'y en a pas (sur ton PC), on garde localhost par sécurité.
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000';
+
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/api/:path*',
+        destination: `${apiUrl}/api/:path*`, // L'adresse devient dynamique !
       }
     ];
   },
