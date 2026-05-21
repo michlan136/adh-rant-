bf4cf29514c553048b81ae2c93d2f7417deccdd0
michlan136 <soulaimanisalma@gmail.com>
2026-05-21 21:01:42 +0100

    Update DATABASE_URL for database connection

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 # ==========================================
 # CONFIGURATION DE LA BASE DE DONNÉES
 # ==========================================
-DATABASE_URL = "postgresql://postgres:michlan@localhost:5432/postgres"
+DATABASE_URL = "postgresql://postgres.xkrutionxnzqkthowair:E6@ZFb!Ra.e3JYb@aws-0-eu-west-1.pooler.supabase.com:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
