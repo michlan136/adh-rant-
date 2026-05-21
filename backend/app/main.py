1f35f42e5b4d18286c10488b73dc22e743ccdf3c
michlan136 <soulaimanisalma@gmail.com>
2026-05-21 16:34:10 +0100

    Update CORS settings to allow all origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -66,7 +66,7 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 # Configuration CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
